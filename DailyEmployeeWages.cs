40b7c2df8424a90865ef82b2279198e58f492fdb
Subhash <barelasubhsh16@gmail.com>
2023-02-09 15:08:45 +0530

    Calculate Daily employee Wages

---
+++ DailyEmployeeWages.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class DailyEmployeeWages
+    {
+        public static void empWage()
+        {
+            int Is_Full_Time = 1;
+            int Emp_Rate_Per_Hour = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = (random.Next(2));
+            if (empHrs == Is_Full_Time)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine(" Employee is Absent.....");
+                empHrs = 0;
+            }
+            empWage = empHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("Employee Wage is :" + empWage);
+        }
+    }
+}
